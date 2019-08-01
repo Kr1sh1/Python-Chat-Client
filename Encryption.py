746a2d67b2ce8bf39bfb2b219d8b7d677385511f
Krishi Wali <krishiwali@hotmail.co.uk>
2019-08-01 16:17:28 +0100

    Removed unused imports
    + minor changes

--- Encryption.py
+++ Encryption.py
@@ -1,19 +1,18 @@
 from cryptography.fernet import Fernet
-import hashlib, os
 
 #An instance of this class is called from another file when data needs to be
 #encrypted/decrypted or hashed.
 class SecretCrypt():
-    def __init__(self, __SecureKey = None):
+    def __init__(self, __SecureKey=None):
 
         #In the constructor we create the key that is used to encrypt and decrypt data.
         #To generate a new key the class must be instanced again.
         #To use an existing key, pass the key as an arguement when calling the class.
-        if __SecureKey == None:
+        if __SecureKey is None:
             self.__SecureKey = Fernet.generate_key()
             self.__SecureKey = Fernet(self.__SecureKey)
             return
-        
+
         self.__SecureKey = Fernet(__SecureKey)
 
     #Function to encrypt data with key
